829657d1932240193898261f9541ac0bc506ab74
Frank Pérez <80561948+Frank-Ish@users.noreply.github.com>
2023-04-13 03:02:15 -0500

    first commit

---
+++ DL/ClientesDL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    internal class ClientesDL
+    {
+        public List<object> getAll()
+        {
+            return null;
+        }
+    }
+}
---
+++ ProyectoINAForms/FrmListaClientes.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ProyectoINAForms
+{
+    partial class FrmListaClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstVClientes = new System.Windows.Forms.ListView();
+            this.colCedula = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colNombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colApellido1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colApellido2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lstVClientes
+            // 
+            this.lstVClientes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colCedula,
+            this.colNombre,
+            this.colApellido1,
+            this.colApellido2});
+            this.lstVClientes.GridLines = true;
+            this.lstVClientes.HideSelection = false;
+            this.lstVClientes.Location = new System.Drawing.Point(2, 0);
+            this.lstVClientes.Name = "lstVClientes";
+            this.lstVClientes.Size = new System.Drawing.Size(725, 438);
+            this.lstVClientes.TabIndex = 0;
+            this.lstVClientes.UseCompatibleStateImageBehavior = false;
+            this.lstVClientes.View = System.Windows.Forms.View.Details;
+            // 
+            // colCedula
+            // 
+            this.colCedula.Text = "Cédula";
+            this.colCedula.Width = 100;
+            // 
+            // colNombre
+            // 
+            this.colNombre.Text = "Nombre";
+            this.colNombre.Width = 150;
+            // 
+            // colApellido1
+            // 
+            this.colApellido1.Text = "Primer Apellido";
+            this.colApellido1.Width = 150;
+            // 
+            // colApellido2
+            // 
+            this.colApellido2.Text = "Segundo Apellido";
+            this.colApellido2.Width = 150;
+            // 
+            // FrmListaClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstVClientes);
+            this.Name = "FrmListaClientes";
+            this.Text = "FrmListaClientes";
+            this.Load += new System.EventHandler(this.FrmListaClientes_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstVClientes;
+        private System.Windows.Forms.ColumnHeader colCedula;
+        private System.Windows.Forms.ColumnHeader colNombre;
+        private System.Windows.Forms.ColumnHeader colApellido1;
+        private System.Windows.Forms.ColumnHeader colApellido2;
+    }
+}
